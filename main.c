d76a45345ce831d92b2bf3cabc6094fe57149510
MartinianoOjeda <martiniano_ojeda@hotmail.com>
2022-07-29 20:11:28 -0300

    creando una matriz

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define MAX 10
+
+typedef char matriz[MAX][MAX];
+
+
+matriz * crearMatriz();
+
+
+int main() {
+
+    return 0;
+}
+
+matriz * crearMatriz(matriz nueva_matriz) {
+
+    int i, j;
+
+    for(i = 0; i < MAX; i++) {
+        for(j = 0; j < MAX; j++) {
+
+        }
+    }
+    return * nueva_matriz;
+}
